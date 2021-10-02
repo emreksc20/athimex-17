9e7eb390845398eb977e4929a9b5a21cc7ef206d
emreksc20 <76550899+emreksc20@users.noreply.github.com>
2021-10-02 17:13:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+window.sr = new ScrollReveal()
+sr.reveal('#item-1-text',{
+  delay:1000,
+  interval:200,
+  distance:"80px",
+  origin:"top"
+}
+); 
+sr.reveal('#item-1-text-1-animated',{
+    delay:1000,
+    interval:200,
+    distance:"180px",
+    origin:"bottom"
+  }
+  );   
+/*FOOTER START*/
+sr.reveal('.footer-text',{
+ duration:1800,
+  distance:"800px",
+  origin:"left"
+}
+);
+sr.reveal('.footer-end-item-1',{
+  delay:1000,
+  rotate: { x:1000, y:0, z:0 },
+  duration: 1000
+ }
+ );
+ sr.reveal('.footer-end-item-2',{
+  delay:1000,
+  rotate: { x:0, y:1000, z:0 },
+  duration: 1000
+ }
+ );
+/* FOOTER END */
+/* menu*/
+// $(document).ready(function() {
+//   $(".yaziacilsin").click(function () { $(".gorunmez").toggle("slow"); });
+// });
+// $(document).ready(function() {
+//   $(".yaziacilsin-2").click(function () { $(".gorunmez-2").toggle("slow"); });
+// });
+// $(document).ready(function() {
+//   $(".yaziacilsin-3").click(function () { $(".gorunmez-3").toggle("slow"); });
+// });
+// $(document).ready(function() {
+//   $(".yaziacilsin-4").click(function () { $(".gorunmez-4").toggle("slow"); });
+// });
+// $(document).ready(function() {
+//   $(".yaziacilsin-5").click(function () { $(".gorunmez-5").toggle("slow"); });
+// });
+// $(document).ready(function() {
+//   $(".yaziacilsin-6").click(function () { $(".gorunmez-6").toggle("slow"); });
+// });
+// $(document).ready(function() {
+//   $(".yaziacilsin-7").click(function () { $(".gorunmez-7").toggle("slow"); });
+// });
